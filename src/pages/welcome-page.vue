395fb87a7fde30c3563e6e18ffb34608a4c1926b
Mira <mira.shenron@gmail.com>
2020-12-01 21:49:11 +0200

    fix

--- src/pages/welcome-page.vue
+++ src/pages/welcome-page.vue
@@ -28,18 +28,54 @@
         </div>
       </div>
       <div class="section-two-container">
-        <div class="section-two flex">
+        <div class="section-two flex justify-center space-between">
           <div class="flex column">
             <h6>All favorite features</h6>
             <h5>
-              Get the same functions<br />
-              with better design
+              Get the same functionality<br />
+              with better design<br />
+              You don`t even need account to start. <br />
+              Explore and enjoy
             </h5>
           </div>
-          <div>Placeholder</div>
+          <img
+            class="img-two"
+            width="450px"
+            src="../assets/imgs/better.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+      <div class="section-three-container">
+        <h6>Core features</h6>
+        <div class="flex">
+          <h5>Redesigned Interface</h5>
+          <span>
+            Designed to put focus on your canvas and creativity. Enjoy an
+            intuitive UI that’s out of your way, keeping you in the zone,
+            working the way you want.
+          </span>
+        </div>
+        <div class="flex">
+          <h5>Advanced Layer System</h5>
+          <span>
+            With PSD compatibility and the ability to export layers as web-ready
+            PNG files, multiple page PDFs, or frames of an animated GIF, PNG or
+            MP4. Pocket gives everybody the power to create stunning graphics on
+            the go.
+          </span>
+        </div>
+        <div class="flex">
+          <h5>Silica-M Painting Engine</h5>
+          <span>
+            Procreate is built from the ground up to give you the most fluid
+            painting experience with beautiful colors, pixel perfect painting,
+            and even better performance than ever before.
+          </span>
         </div>
       </div>
     </div>
+
     <footer class="footer">Footer Placeholder</footer>
   </section>
 </template>
