47fb75d2981b88ed33226bc1edb3979657bcf493
moshekalman <mosheK.r8@gmail.com>
2020-12-07 19:44:42 +0200

    Whats good

--- src/main.js
+++ src/main.js
@@ -5,23 +5,26 @@ import router from './router/index';
 import store from './store/index';
 import './styles/main.scss';
 import ElementUI from 'element-ui';
-import locale from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale/lang/en';
 import 'element-ui/lib/theme-chalk/index.css';
 import vClickOutside from 'v-click-outside';
 import VueInitialsImg from 'vue-initials-img';
 import moment from 'moment';
-import 'animate.css'
+import 'animate.css';
 
 
-Vue.prototype.moment = moment
-Vue.use(moment)
-Vue.use(vClickOutside)
-Vue.use(ElementUI, { locale })
-Vue.use(VueInitialsImg)
+Vue.prototype.moment = moment;
+Vue.use(moment);
+Vue.use(vClickOutside);
+Vue.use(ElementUI, { locale });
+Vue.use(VueInitialsImg);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  created() { 
+    document.title = 'Thriller'
+  },
   render: h => h(app)
 }).$mount('#app');
